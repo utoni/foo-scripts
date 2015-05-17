46d57fad0b8574ad1f790a2144f9172380967c3e
toni <toni@devlap.local>
2015-05-17 20:28:21 +0200

    fixed msi stuff, added io usage

---
+++ configs/xmobar_iostat.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+OUT=$(dstat -dD total --noheaders --noupdate --integer 1 1 | tail -n 1)
+# | sed -n 's/^\s*\([0-9]*[[:alpha:]]*\).*$/__\1___/p'
+
+READ=$(echo $OUT | sed -n 's/^\s*\([0-9]*[[:alpha:]]*\).*$/\1/p')
+WRITE=$(echo $OUT | sed -n 's/^\s*[0-9]*[[:alpha:]]*\s*\([0-9]*[[:alpha:]]*\)$/\1/p')
+
+
+outio() {
+local lc
+lc=$(echo "$1" | cut -c $((${#1})))
+if [ $lc = "k" ]; then
+	echo -n '<fc=#FFFF00>'
+elif [ $lc = "B" ]; then
+	echo -n '<fc=#00FF00>'
+elif [ $lc = "M" ]; then
+	echo -n '<fc=#FF0000>'
+else
+	echo -n '<fc=#FFFFFF>'
+fi
+echo -n "${1}</fc>"
+}
+
+outio "${READ}"
+echo -n ' | '
+outio "${WRITE}"
+echo
+
+exit $?
