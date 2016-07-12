d89d3bd711823a20b4d15fbec375dc035f998788
toni <matzeton@googlemail.com>
2016-07-12 14:58:42 +0200

    irssi ircs not supported from command line :/

--- irssi-tor.sh
+++ irssi-tor.sh
@@ -8,7 +8,7 @@ while `netstat -l4n | grep -qoE "^tcp(.*):${SOC_PORT}"`; do
 	SOC_PORT=${NEXT_PORT}
 done
 
-if [ $# -ne 3 ]; then
+if [ $# -ne 3 ] && [ $# -ne 0 ]; then
 	echo "$0: [IRC_HOST] [IRC_PORT] [IRC_NICK]"
 	exit 1
 fi
@@ -17,10 +17,14 @@ IRC_PORT="$2"
 IRC_NICK="$3"
 
 sudo iptables -A INPUT -p tcp '!' -s 127.0.0.1 --dport ${SOC_PORT} -j REJECT
-socat TCP4-LISTEN:${SOC_PORT},reuseaddr,range=127.0.0.1/32 SOCKS4A:localhost:${IRC_HOST}:${IRC_PORT},socksport=9050 &
+socat -d -d -lmlocal2 TCP4-LISTEN:${SOC_PORT},reuseaddr,fork,range=127.0.0.1/32 SOCKS4A:localhost:${IRC_HOST}:${IRC_PORT},socksport=9050 &
 SOC_PID=$!
 echo "socat pid ${SOC_PID}"
-irssi -c localhost -p ${SOC_PORT} -n ${IRC_NICK}
+if [ $# -eq 0 ]; then
+	irssi --noconnect
+else
+	irssi -c localhost -p ${SOC_PORT} -n ${IRC_NICK}
+fi
 echo "kill ${SOC_PID}"
 kill -SIGTERM ${SOC_PID} 2>/dev/null
 wait ${SOC_PID}
