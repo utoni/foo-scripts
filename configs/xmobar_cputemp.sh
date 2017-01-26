7fabe28b9f468f6bc5ca286e14ea94f4173a0a8a
toni <matzeton@googlemail.com>
2017-01-27 00:42:05 +0100

    xmobar net position, cputemp.sh color fix

--- configs/xmobar_cputemp.sh
+++ configs/xmobar_cputemp.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 if [ "x$1" = "xCPU" ]; then
-	OUT=$(sensors | sed -n 's/^CPU Temperature:\s*+\(.*\)°C\s*(\(.*\)/\1/p')
+	OUT=$(sensors | sed -n 's/^CPU Temperature:\s*+\(.*\)\..*°C\s*(\(.*\)/\1/p')
 elif [ "x$1" = "xMB" ]; then
-	OUT=$(sensors | sed -n 's/^MB Temperature:\s*+\(.*\)°C\s*(\(.*\)/\1/p')
+	OUT=$(sensors | sed -n 's/^MB Temperature:\s*+\(.*\)\..*°C\s*(\(.*\)/\1/p')
 else
 	exit 1
 fi
 
-if [ $OUT -ge 60 ]; then
-	echo "<fc=#00FF00>$OUT</fc>°C"
-elif [ $OUT -ge 67 ]; then
-	echo "<fc=#FFFF00>$OUT</fc>°C"
-elif [ $OUT -ge 75 ]; then
-	echo "<fc=#FF0000>$OUT</fc>°C"
-else
+if [ $OUT -le 60 ]; then
 	echo "$OUT°C"
+elif [ $OUT -le 75 ]; then
+	echo "<fc=#00FF00>$OUT</fc>°C"
+elif [ $OUT -le 85 ]; then
+	echo "<fc=#FFFF00>$OUT</fc>°C"
+else
+	echo "<fc=#FF0000>$OUT</fc>°C"
 fi
