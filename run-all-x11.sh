dd66f600505ef7d208cf010e4b4e3d29eb8e3fa3
Toni Uhlig <matzeton@googlemail.com>
2017-07-23 08:55:37 +0200

    .

--- run-all-x11.sh
+++ run-all-x11.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TERMAPP='x-terminal-emulator'
-for wid in $(xdotool search --onlyvisible --sync --all --class ${TERMAPP})
-  do xdotool type --window $wid "$*"
-     xdotool key --window $wid KP_Enter
+for wid in $(xdotool search --onlyvisible --sync --all --class ${TERMAPP}); do
+	xdotool type --window $wid "$*"
+	xdotool key --window $wid KP_Enter
 done
