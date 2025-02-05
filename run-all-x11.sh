9aa5cd79f20b09942dff9e2bbf696b1dc60a74dd
Toni Uhlig <matzeton@googlemail.com>
2025-02-05 16:05:34 +0100

    updated run-all-x11 script
    Signed-off-by: Toni Uhlig <matzeton@googlemail.com>

--- run-all-x11.sh
+++ run-all-x11.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-TERMAPP='x-terminal-emulator'
+TERMAPP='lxterminal'
+CURRENT_WINDOW_FOCUS=$(xdotool getwindowfocus)
 for wid in $(xdotool search --onlyvisible --sync --all --class ${TERMAPP}); do
+	xdotool windowfocus --sync $wid
 	xdotool type --window $wid "$*"
 	xdotool key --window $wid KP_Enter
 done
+xdotool windowfocus --sync ${CURRENT_WINDOW_FOCUS}
